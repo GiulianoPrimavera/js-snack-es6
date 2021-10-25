7ac1af27560f7266ab8ac6419c993523ec776af7
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-25 15:59:45 +0200

    adds a function that destructure the original array and creates a new one with only the names and fouls

--- js/squadra_calcio.js
+++ js/squadra_calcio.js
@@ -2,6 +2,7 @@
 /* 
 -creare array di oggetti (squadre di calcio)
 -assegnare numeri random per punti e falli subiti
+-creare array contentente solo nomi e falli subiti
 */
 
 const squadre = [
@@ -38,13 +39,25 @@ const squadre = [
 }   
 //questa funzione serve per assegnare i punti e i falli subiti per ogni squadra in maniera randomica
 function randomPointFauls(){
-    console.log("squadre prima", squadre);
     for (let i = 0; i < squadre.length; i++) {
-        squadre[i].puntiFatti = getRandomNumber();
-        
-        squadre[i].falliSubiti = getRandomNumber();
+        squadre[i].puntiFatti = getRandomNumber(20, 0);
+        squadre[i].falliSubiti = getRandomNumber(20, 0);
     }
     console.log("squadre dopo", squadre);
 }               
 randomPointFauls()
+
+//destrutturo l'array delle squadre e creo un array contenente un oggetto per squadra contenente solo il nome e i falli subiti
+function desctructuringAndStamp(){
+    const arrayNomiFalli = [];
+    for (let i = 0; i < squadre.length; i++){
+        //recupero solo i nomi e i falli subiti di ogni oggetto
+        const {nome, falliSubiti} = squadre[i];
+        //popolo l'array
+        arrayNomiFalli.push({nome, falliSubiti});
+    }
+    console.log(arrayNomiFalli);
+}
+desctructuringAndStamp()
+
 
